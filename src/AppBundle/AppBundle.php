e703869cf0f81a1e411f463ba30ceda0e251ca78
Ilya Ukrainskiy <kilinsight@mail.ru>
2019-07-01 15:46:44 +0700

    in-progress

---
+++ src/AppBundle/AppBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle for entities, documents and core functionality
+ *
+ * @package AppBundle
+ */
+class AppBundle extends Bundle
+{
+}
